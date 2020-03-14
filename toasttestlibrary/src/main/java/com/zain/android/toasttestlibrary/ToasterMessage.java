1ef5bec2f8352241e66e7d1123b8142404b79798
Zain <m7mmd.zain@gmail.com>
2020-03-14 21:24:01 +0200

    adding non static method

--- toasttestlibrary/src/main/java/com/zain/android/toasttestlibrary/ToasterMessage.java
+++ toasttestlibrary/src/main/java/com/zain/android/toasttestlibrary/ToasterMessage.java
@@ -5,9 +5,12 @@ import android.widget.Toast;
 
 public class ToasterMessage {
 
-    public static void s(Context c, String message){
+    public static void s(Context c, String message) {
+        Toast.makeText(c, message, Toast.LENGTH_SHORT).show();
 
-        Toast.makeText(c,message,Toast.LENGTH_SHORT).show();
+    }
 
+    public void showToast(Context c, String message){
+        Toast.makeText(c, message, Toast.LENGTH_SHORT).show();
     }
 }
